28d5e19ee1cee3ff8f36ba9f1f152ab9c49dcb65
korur <serdar.korur@gmail.com>
2021-11-30 10:09:00 +0100

    add netlify adapter and toml, fix svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-
 /** @type {import('@sveltejs/kit').Config} */
+
+import adapter from '@sveltejs/adapter-netlify'
+
 const config = {
+
 	kit: {
 		adapter: adapter(),
-
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		prerender: {
+            crawl: true,
+            enabled: true
+        },
+		files: {
+			assets: 'static'
+		}
 	}
 };
 
