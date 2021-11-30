48201bdfaf6e3ff02d67618945dc62b4c9df17b9
korur <serdar.korur@gmail.com>
2021-11-30 12:47:18 +0100

    fix heroicons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -79,7 +79,7 @@ function handleClick() {
                                 <span class='block text-gray-500 text-sm'>Recipe by Mario</span>
                             </div>
                             <div class="badge">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-1 h-1 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                                   </svg>
                                 <span>
